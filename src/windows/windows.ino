27be51206a8126f9fea6807fc598dff6027b65ab
Julien Dufour <git@trazere.com>
2019-06-19 02:48:19 +0200

    Revamped the code to use the Window class

--- src/windows/windows.ino
+++ src/windows/windows.ino
@@ -1,38 +1,37 @@
+
 // Open or close all 8 green house windows when internal temperature is higher
 // or lower than a fixed threshold.
 
 // History:
 // 2016-04-01 Yoann Aubineau - Initial program
 // 2016-08-16 Yoann Aubineau - Code cleanup before pushing to Github
+// 2019-06-19 Julien Dufour - Revamped the code with the Window class
 
 // Todo:
 // - Make board LED blink when temperature is between thredsholds.
-// - This code is pretty low-lever and tedious to follow. Introducing a higher
-//   level `Window` class could improve readability substantially.
+// - Add manual open/close control.
 
 #include "DHT.h"
+#include "Window.h"
 
 // ****************************************************************************
-// WINDOWS                                     N1, N2, N3, N4, S1, S2, S3, S4
+// Windows
 
 const int window_count = 8;
-#define FOR_EACH_WINDOW for (int i = 0; i < window_count; i++)
+#define FOR_EACH_WINDOW for (int i = 0; i < window_count; i += 1)
 
-const int win_direction_pins[window_count] = { 22, 24, 26, 28, 30, 32, 34, 36 };
-const int win_action_pins[window_count]    = { 38, 40, 42, 44, 46, 48, 50, 52 };
-const int win_opened_pins[window_count]    = { 39, 41, 43, 45, 47, 49, 51, 53 };
-const int win_closed_pins[window_count]    = { 23, 25, 27, 29, 31, 33, 35, 37 };
-
-int win_status[window_count]               = {  0,  0,  0,  0,  0,  0,  0,  0 };
-
-unsigned long win_started_at[window_count] = {  0,  0,  0,  0,  0,  0,  0,  0 };
 const unsigned long win_max_running_time = 30000;
 
-// ****************************************************************************
-// MESSAGE buffer used to delay writing to serial, which is slow
-
-String messages[window_count];
-const String NULL_STRING = String("NULL_STRING");
+Window windows[window_count] = {
+  Window("N1", 22, 38, 39, 23, win_max_running_time),
+  Window("N2", 24, 40, 41, 25, win_max_running_time),
+  Window("N3", 26, 42, 43, 27, win_max_running_time),
+  Window("N4", 28, 44, 45, 29, win_max_running_time),
+  Window("S1", 30, 46, 47, 31, win_max_running_time),
+  Window("S2", 32, 48, 49, 33, win_max_running_time),
+  Window("S3", 34, 50, 51, 35, win_max_running_time),
+  Window("S4", 36, 52, 53, 37, win_max_running_time),
+};
 
 // ****************************************************************************
 // TEMPERATURE
@@ -65,87 +64,23 @@ void setup() {
   dht.begin();
 
   FOR_EACH_WINDOW {
-    digitalWrite(win_direction_pins[i], LOW);
-    digitalWrite(win_action_pins[i], LOW);
-    pinMode(win_direction_pins[i], OUTPUT);
-    pinMode(win_action_pins[i], OUTPUT);
-    pinMode(win_opened_pins[i], INPUT_PULLUP);
-    pinMode(win_closed_pins[i], INPUT_PULLUP);
-    win_status[i] = 0;
+    windows[i].setup();
   }
 }
 
 // ****************************************************************************
 void loop() {
-
+  
   // --------------------------------------------------------------------------
-  // Stop motors after timeout
-
+  // Autostop the windows.
+  
   FOR_EACH_WINDOW {
-    messages[i] = NULL_STRING;
+    windows[i].autostop();
   }
-  FOR_EACH_WINDOW {
-    if (win_status[i] != 0 &&
-        digitalRead(win_action_pins[i]) == HIGH &&
-        win_started_at[i] + win_max_running_time <= millis()) {
-      messages[i] = "Window " + String(i) + " timeout --> stopping motor.";
-      digitalWrite(win_action_pins[i], LOW);
-      digitalWrite(win_direction_pins[i], LOW);
-    }
-  }
-  FOR_EACH_WINDOW {
-    if (messages[i] != NULL_STRING) {
-      Serial.println(messages[i]);
-    }
-  }
-
-  // --------------------------------------------------------------------------
-  // Stop motors when windows are fully opened
-
-  FOR_EACH_WINDOW {
-    messages[i] = NULL_STRING;
-  }
-  FOR_EACH_WINDOW {
-    if (win_status[i] == 1 &&
-        digitalRead(win_action_pins[i]) == HIGH &&
-        digitalRead(win_direction_pins[i]) == LOW &&
-        digitalRead(win_opened_pins[i]) == LOW) {
-      messages[i] = "Window " + String(i) + " fully opened --> stopping motor.";
-      digitalWrite(win_action_pins[i], LOW);
-      digitalWrite(win_direction_pins[i], LOW);
-    }
-  }
-  FOR_EACH_WINDOW {
-    if (messages[i] != NULL_STRING) {
-      Serial.println(messages[i]);
-    }
-  }
-
-  // --------------------------------------------------------------------------
-  // Stop motors when windows are fully closed
-
-  FOR_EACH_WINDOW {
-    messages[i] = NULL_STRING;
-  }
-  FOR_EACH_WINDOW {
-    if (win_status[i] == -1 &&
-        digitalRead(win_action_pins[i]) == HIGH &&
-        digitalRead(win_direction_pins[i]) == HIGH &&
-        digitalRead(win_closed_pins[i]) == LOW) {
-      messages[i] = "Window " + String(i) + " fully closed --> stopping motor.";
-      digitalWrite(win_action_pins[i], LOW);
-      digitalWrite(win_direction_pins[i], LOW);
-    }
-  }
-  FOR_EACH_WINDOW {
-    if (messages[i] != NULL_STRING) {
-      Serial.println(messages[i]);
-    }
-  }
-
+  
   // --------------------------------------------------------------------------
   // Read temperature at fixed interval
-
+  
   unsigned long current_timestamp = millis();
   if (temperature_last_read_at + temperature_read_interval > current_timestamp) {
     return;
@@ -164,20 +99,8 @@ void loop() {
   if (current_temperature > high_temperature_threshold) {
     BOARD_LED_ON
     FOR_EACH_WINDOW {
-      messages[i] = NULL_STRING;
-    }
-    FOR_EACH_WINDOW {
-      if (win_status[i] != 1 && digitalRead(win_opened_pins[i]) != LOW) {
-        win_status[i] = 1;
-        messages[i] = "OPENING window " + String(i);
-        digitalWrite(win_direction_pins[i], LOW);
-        digitalWrite(win_action_pins[i], HIGH);
-        win_started_at[i] = millis();
-      }
-    }
-    FOR_EACH_WINDOW {
-      if (messages[i] != NULL_STRING) {
-        Serial.println(messages[i]);
+      if (!windows[i].hasTimedOut()) {
+        windows[i].open();
       }
     }
     return;
@@ -189,20 +112,8 @@ void loop() {
   if (current_temperature < low_temperature_threshold) {
     BOARD_LED_OFF
     FOR_EACH_WINDOW {
-      messages[i] = NULL_STRING;
-    }
-    FOR_EACH_WINDOW {
-      if (win_status[i] != -1 && digitalRead(win_closed_pins[i]) != LOW) {
-        win_status[i] = -1;
-        messages[i] = "CLOSING window " + String(i);
-        digitalWrite(win_direction_pins[i], HIGH);
-        digitalWrite(win_action_pins[i], HIGH);
-        win_started_at[i] = millis();
-      }
-    }
-    FOR_EACH_WINDOW {
-      if (messages[i] != NULL_STRING) {
-        Serial.println(messages[i]);
+      if (!windows[i].hasTimedOut()) {
+        windows[i].close();
       }
     }
     return;
@@ -210,23 +121,10 @@ void loop() {
 
   // --------------------------------------------------------------------------
   // Stop windows otherwise
-
+  
   BOARD_LED_OFF
   FOR_EACH_WINDOW {
-    messages[i] = NULL_STRING;
-  }
-  FOR_EACH_WINDOW {
-    if (win_status[i] != 0) {
-      win_status[i] = 0;
-      messages[i] = "STOPPING window " + String(i);
-      digitalWrite(win_action_pins[i], LOW);
-      digitalWrite(win_direction_pins[i], LOW);
-    }
-  }
-  FOR_EACH_WINDOW {
-    if (messages[i] != NULL_STRING) {
-      Serial.println(messages[i]);
-    }
+    windows[i].stop();
   }
   return;
 }
