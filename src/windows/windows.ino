67f4ac989a6dcfdc4bdbc9c14ba0c4cd7c6e3917
Julien Dufour <git@trazere.com>
2019-07-07 02:37:56 +0200

    Added support for manual control of the windows.

--- src/windows/windows.ino
+++ src/windows/windows.ino
@@ -6,9 +6,9 @@
 // 2016-04-01 Yoann Aubineau - Initial program
 // 2016-08-16 Yoann Aubineau - Code cleanup before pushing to Github
 // 2019-06-19 Julien Dufour - Revamped the code with the Window class
+// 2019-07-07 Julien Dufour - Added support for manual control
 
 // Todo:
-// - Add manual open/close control.
 
 #include "DHT.h"
 #include "Window.h"
@@ -58,6 +58,26 @@ void blinkLed() {
 
 
 // ****************************************************************************
+// Manual control
+
+const int manualOpenPin = 8;
+const int manualClosePin = 9;
+
+void setupManualControl() {
+  pinMode(manualOpenPin, INPUT_PULLUP);
+  pinMode(manualClosePin, INPUT_PULLUP);
+}
+
+bool isManuallyOpening() {
+  return LOW == digitalRead(manualOpenPin);
+}
+
+bool isManuallyClosing() {
+  return LOW == digitalRead(manualClosePin);
+}
+
+
+// ****************************************************************************
 // Temperature
 
 const int SENSOR_PIN = A0;
@@ -67,18 +87,27 @@ DHT dht(SENSOR_PIN, SENSOR_TYPE);
 const float closeTemperatureThreshold = 24.0;
 const float openTemperatureThreshold = 26.0;
 
-const unsigned long temperatureReadInterval = 2000;
-unsigned long lastTemperatureReadTime = 0;
+const unsigned long temperatureLogInterval = 2000;
+unsigned long lastTemperatureLogTime = 0;
 
 
 // ****************************************************************************
 // Sketch
+
+#define IDLE_STATE 0
+#define MANUAL_OPEN_STATE 1
+#define MANUAL_CLOSE_STATE 2
+#define TEMP_OPEN_STATE 3
+#define TEMP_CLOSE_STATE 4
+
+int state = IDLE_STATE;
 
 void setup() {
   Serial.begin(9600);
   Serial.flush();
   
   setupLed();
+  setupManualControl();
   
   dht.begin();
   
@@ -93,45 +122,94 @@ void loop() {
     windows[i].autostop();
   }
   
-  // Read temperature at fixed interval.
-  unsigned long now = millis();
-  if (lastTemperatureReadTime + temperatureReadInterval > now) {
-    return;
-  }
-  float temperature = dht.readTemperature();
-  lastTemperatureReadTime = now;
-  if (isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
-  } else {
-    Serial.println(String(temperature) + " °C");
-    
-    // Open the windows when temperature is high.
-    if (temperature > openTemperatureThreshold) {
-      switchLedOn();
-      
-      FOR_EACH_WINDOW {
-        if (!windows[i].hasTimedOut()) {
-          windows[i].open();
-        }
-      }
-    
-    // Close the windows when temperature is low.
-    } else if (temperature < closeTemperatureThreshold) {
-      switchLedOff();
-      
-      FOR_EACH_WINDOW {
-        if (!windows[i].hasTimedOut()) {
-          windows[i].close();
-        }
-      }
   
-    // Stop the windows otherwise.
-    } else {
-      blinkLed();
+  // Read the controls.
+  bool manualOpen = isManuallyOpening();
+  bool manualClose = isManuallyClosing();
+  float temperature = dht.readTemperature();
+  
+  
+  // Log the temperature.
+  unsigned long now = millis();
+  if (now >= lastTemperatureLogTime + temperatureLogInterval) {
+    if (!isnan(temperature)) {
+      Serial.println(String(temperature) + " °C");
       
+      if (temperature > openTemperatureThreshold) {
+        switchLedOn();     
+      } else if (temperature < closeTemperatureThreshold) {
+        switchLedOff();
+      } else {
+        blinkLed();
+      }
+    } else {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    
+    lastTemperatureLogTime = now;
+  }
+
+  
+  // Manually open the windows.
+  if (manualOpen) {
+    if (MANUAL_OPEN_STATE != state) {
+      Serial.println("Manually opening all windows.");
+      
+      FOR_EACH_WINDOW {
+        windows[i].open();
+      }
+      
+      state = MANUAL_OPEN_STATE;
+    }
+
+  // Manually close the windows.
+  } else if (manualClose) {
+    if (MANUAL_CLOSE_STATE != state) {
+      Serial.println("Manually closing all windows.");
+      
+      FOR_EACH_WINDOW {
+        windows[i].close();
+      }
+      
+      state = MANUAL_CLOSE_STATE;
+    }
+
+  // Stop the windows when temperature is unavailable.
+  } else if (isnan(temperature)) {
+    if (MANUAL_OPEN_STATE == state || MANUAL_CLOSE_STATE == state) {
       FOR_EACH_WINDOW {
         windows[i].stop();
       }
+      
+      state = IDLE_STATE;
+    }
+  
+  // Open the windows when the temperature is high.
+  } else if (temperature > openTemperatureThreshold) {
+    if (TEMP_OPEN_STATE != state) {
+      Serial.println("Temperature is high, opening all windows.");
+      
+      FOR_EACH_WINDOW {
+        windows[i].open();
+      }
+      
+      state = TEMP_OPEN_STATE;
+    }
+    
+  // Close the windows when the temperature is low.
+  } else if (temperature < closeTemperatureThreshold) {
+    if (TEMP_CLOSE_STATE != state) {
+      Serial.println("Temperature is low, closing all windows.");
+      
+      FOR_EACH_WINDOW {
+        windows[i].close();
+      }
+
+      state = TEMP_CLOSE_STATE;
     }
   }
+
+
+  // Wait a little
+  delay(100);
 }
